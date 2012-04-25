10abe9f12f0ee24cbcb35af39eae0ae990942ed6
mocomatics <mocomatics@gmail.com>
2012-04-25 20:34:43 +0100

    added mesh displacement and a few other things ;)

--- js/Hokusai.js
+++ js/Hokusai.js
@@ -1,93 +1,36 @@
-function Hokusai (width) {
+function Hokusai (width, subs) {
 	var scope  = this;
+
+	THREE.Geometry.call( this );
 	var w = width;
+
+	var r = 1.42; //A4 ratio
+	var h = w*r;
+
 	
-	this.draw = function (){
+	for (var i=0; i<subs; i++){
 
-		w = width;
-		var r = 1.41; //A4 ratio
-		var h = w*r;
-		var k = 4; //subdivisions y
-
-		//sheet of A4
-		v3( -w/2, -h/2,  0 ); //0
-		v3(  w/2, -h/2,  0 ); //1
-
-
-		for (var i=0; i<k; i++){
-			v3( -w/2, -h/2+h/k*i,  0 );
-			v3(  w/2, -h/2+h/k*i,  0 );
-		}
-
-		f3 (0,1,2);
-		f3 (0,2,3);
-
-		f3 (3,2,4);
-		f3 (3,4,5);
-
-		f3 (5,4,6);
-		f3 (5,6,7);
-
-		f3 (7,6,8);
-		f3 (7,8,9);
-
-
-		//v3(  w/2,  w/2,  0 );
-		//v3( -w/2,  w/2,  0 );
-
-
-
-		//flexible paper sheets
-		//fold.y = Math.sin(counter)*10;
-
-		/*
-		var vStart = v;
-		for (var k = 0; k <steps; k++){
-			fold.z = k/steps*h/steps;
-			//fold.normalize();
-			//fold.multiplyScalar(h/steps);
-			v.addSelf(fold);
-			v3(v);
-		}
-
-		v.x = v.x -w;
-		v3(v);
-
-		for (var k = 0; k < steps; k++){
-			fold.z = (9-k)/steps*h/steps;
-			//fold.normalize();
-			//fold.multiplyScalar(h/steps);
-			v.subSelf( fold);
-			v3(v);
-		}
-
-		v.x = vStart.x+w;
-		v.y = vStart.y;
-		v.z = vStart.z;
-		v3(v);*/
-		
-
-
-
-		f3(0,1,2);
-		f3(0,2,3);
-
-
-		//this.computeCentroids();
-		this.computeFaceNormals();
+		v3( -w/2, -h/2+h*i/subs,  0 );
+		v3(  w/2, -h/2+h*i/subs,  0 );
 	}
 
-	function v3 (x,y,z){
-			scope.vertices.push( new THREE.Vector3(x,y,z));
-		}
-	function f3 (a,b,c){
-			scope.faces.push( new THREE.Face3(a,b,c) );
-		}
-	
+	for (var i=0; i< subs*2-2; i++){
+			//console.log ("Hokusai: add faces: "+i);
+			f3 (i, i+1, i+2);
+	}
 
-	//SETUP
-	THREE.Geometry.call( this );
-	this.draw();
+	//console.log ("Hokusai: this.vertices.length: " + this.vertices.length);
+	//console.log ("Hokusai: this.faces.length: " + this.faces.length);
+
+	this.computeFaceNormals();
+
+
+	function v3 (x,y,z){
+		scope.vertices.push( new THREE.Vector3(x,y,z));
+	}
+	function f3 (a,b,c){
+		scope.faces.push( new THREE.Face3(a,b,c) );
+	}
 
 }
 
--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@ $(function() {
 
   var mouse2D = { x: 0, y: 0 },
 
-  PARTICLES_COUNT = 10, 
-  SHEET_WIDTH = 10,
+  PARTICLES_COUNT = 40, 
+  SHEET_WIDTH = 6,
+  SHEET_SUBDIVISIONS = 10,
   //START_POSITION = new THREE.Vector3 (-40,-25,0),
 
   SCREEN_WIDTH = $(".container").width(),
@@ -13,8 +14,8 @@ $(function() {
 
   camera, scene, renderer, projector, orbitRadius, theta, counter, particles, sheets;
 
-  window.DRAG = .92;
-  window.GRAVITY = .03;
+  window.DRAG = .95;
+  window.GRAVITY = .01;
 
   init();
   animate();
@@ -27,7 +28,7 @@ $(function() {
    
     //CAMERA
     camera = new THREE.PerspectiveCamera( 35, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 10000 );
-    camera.position.y = 10;
+    camera.position.y = -5;
     camera.position.x = 0;
     camera.position.z = 150;
     scene.add( camera );
@@ -57,23 +58,32 @@ $(function() {
     counter = 0;
 
     var Particle = function () {
+      this.ID =0;
       this.position = new THREE.Vector3();
       this.velocity = new THREE.Vector3();
       this.rotation = new THREE.Vector3();
       this.seed = Math.random();
       this.goal = new THREE.Vector3 (100,0,0);
       this.running = false;
-      this.ID =0;
+      this.displace = false;
+      this.decay = 1;
 
       var  _width = 500, _height = 500, _depth = 200;
       var _acceleration = new THREE.Vector3();
       var _maxSpeed = 4;
-      var _avoidWalls = true;
+      var _avoidWalls = false;
 
       this.setGoal = function ( target ) {
 
          _goal = target;
 
+      }
+      this.setDisplace = function (bool) {
+        displace = bool;
+      }
+
+      this.setblowAway = function (bool) {
+        this.blowAway = bool;
       }
 
       this.setWorldSize = function ( width, height, depth ) {
@@ -84,9 +94,9 @@ $(function() {
 
       }
 
-      this.setAvoidWalls = function ( value ) {
+      this.setAvoidWalls = function ( bool ) {
 
-        _avoidWalls = value;
+        _avoidWalls = bool;
 
       }
       /*
@@ -124,16 +134,16 @@ $(function() {
           this.velocity.divideScalar( l / _maxSpeed );
         }
         // fake air resistance
-        this.velocity.multiplyScalar(window.DRAG);
+        this.velocity.multiplyScalar(window.DRAG + this.seed*.03);
 
         this.position.addSelf( this.velocity );
         _acceleration.set( 0, 0, 0 );
         
       }
       this.gravitate = function () {
-        _acceleration.addSelf (new THREE.Vector3(0,-window.GRAVITY,0));
+        _acceleration.addSelf (new THREE.Vector3(0,-window.GRAVITY*this.decay,0));
       }
-
+      /*
       this.PerlinMovement = function () {
 
         var x = this.position.x % SCREEN_WIDTH; 
@@ -157,20 +167,19 @@ $(function() {
        var speed = 1;
        var driver = (timeOffset+counter)/speed;
        //blow particles up
-       _acceleration.set(Math.sin(driver)/2, Math.cos(driver)/2+1, 0);
+       _acceleration.set(Math.sin(driver)/2*this.decay, (Math.cos(driver)/2+1)*this.decay, 0);
        // _acceleration.multiplyScalar( 1);
       }
-
-      this.setblowAway = function (value) {
-        this.blowAway = value;
-      }
+    
+      
 
       this.rotate = function () {
-        this.rotation.x += .01;
-        this.rotation.y += .01;
-        this.rotation.z += .01;
-      }
+        this.rotation.x += .01 * this.decay;
+        this.rotation.y += .01 * this.decay;
+        this.rotation.z += .01 * this.decay;
 
+      }
+*/
       this.PerlinRotate = function () {
         // doesn't work
         var x = this.position.x % SCREEN_WIDTH; 
@@ -180,11 +189,11 @@ $(function() {
         var rx = PerlinNoise.noise( size*x, 1, .1);
         var ry = PerlinNoise.noise( 1, size*y, .1);
        //console.log("PerlinRotate: rx: "+rx + " ry: "+ry);
-        var speed =.09; //*this.velocity;
+        var speed =.09 * this.seed; //*this.velocity;
 
-        this.rotation.x += rx*speed;//(Math.sin(counter/10)*_seed+_seed/2)/2;
-        this.rotation.y += ry*speed;;//(Math.sin(counter/10)*_seed+_seed/2)/2;
-        //this.rotation.z += speed * Math.sin(counter/10)*this.seed+this.seed/2);
+        this.rotation.x += rx*speed *this.decay;
+        this.rotation.y += ry*speed *this.decay; 
+        this.rotation.z += .02 * this.seed * this.decay;
 
       }
 
@@ -209,83 +218,95 @@ $(function() {
 
       this.run = function ( particles ) {
 
-
-        if (this.blowAway && counter/10%(this.ID+1)==0) {
+        // blow away
+        // one after the other
+        // many in the beginning
+        if (this.blowAway && (counter*30-counter) % (this.ID+1) == 0) {
             //console.log ("Particle.run() : counter: "+counter);
             this.blowAway = false;
             this.running = true;
-           // console.log("Particle.run() : this.ID: "+this.ID);
+           
+            //console.log("Particle.run() : this.ID: "+this.ID);
             // GUSH
-           _acceleration.set( Math.random()*20, Math.random()*10, 0);
+            var xSpread = 10;
+            var ySpread = 5;
+            var xGush = (Math.random()+.5)*xSpread + (this.ID+1) / PARTICLES_COUNT;
+            var yGush = (Math.random()+.5)*ySpread + (this.ID+1) / PARTICLES_COUNT;
+           // console.log("Particle.run() : xGush "+xGush + " yGush "+yGush);
+            this.velocity.set( xGush, yGush, (Math.random()+.5)*4);
+            this.setDisplace(true);
         }
 
 
         if (this.running){
         //this.PerlinMovement();
 
-        if ( _avoidWalls ) {
-          var vector = new THREE.Vector3();
-          vector.set( - _width, this.position.y, this.position.z );
-          vector = this.avoid( vector );
-          vector.multiplyScalar( 5 );
-          _acceleration.addSelf( vector );
+          if ( _avoidWalls ) {
+            var vector = new THREE.Vector3();
+            vector.set( - _width, this.position.y, this.position.z );
+            vector = this.avoid( vector );
+            vector.multiplyScalar( 5 );
+            _acceleration.addSelf( vector );
 
-          vector.set( _width, this.position.y, this.position.z );
-          vector = this.avoid( vector );
-          vector.multiplyScalar( 5 );
-          _acceleration.addSelf( vector );
+            vector.set( _width, this.position.y, this.position.z );
+            vector = this.avoid( vector );
+            vector.multiplyScalar( 5 );
+            _acceleration.addSelf( vector );
 
-          vector.set( this.position.x, - _height, this.position.z );
-          vector = this.avoid( vector );
-          vector.multiplyScalar( 5 );
-          _acceleration.addSelf( vector );
+            vector.set( this.position.x, - _height, this.position.z );
+            vector = this.avoid( vector );
+            vector.multiplyScalar( 5 );
+            _acceleration.addSelf( vector );
 
-          vector.set( this.position.x, _height, this.position.z );
-          vector = this.avoid( vector );
-          vector.multiplyScalar( 5 );
-          _acceleration.addSelf( vector );
+            vector.set( this.position.x, _height, this.position.z );
+            vector = this.avoid( vector );
+            vector.multiplyScalar( 5 );
+            _acceleration.addSelf( vector );
 
-          vector.set( this.position.x, this.position.y, - _depth );
-          vector = this.avoid( vector );
-          vector.multiplyScalar( 5 );
-          _acceleration.addSelf( vector );
+            vector.set( this.position.x, this.position.y, - _depth );
+            vector = this.avoid( vector );
+            vector.multiplyScalar( 5 );
+            _acceleration.addSelf( vector );
 
-          vector.set( this.position.x, this.position.y, _depth );
-          vector = this.avoid( vector );
-          vector.multiplyScalar( 5 );
-          _acceleration.addSelf( vector );
+            vector.set( this.position.x, this.position.y, _depth );
+            vector = this.avoid( vector );
+            vector.multiplyScalar( 5 );
+            _acceleration.addSelf( vector );
 
-        }
+          }
+          //this.followMouse();
+          this.PerlinRotate();
+          this.move();
+          this.gravitate();
 
-       //this.followMouse();
-        this.PerlinRotate();
-        this.move();
-        this.gravitate();
+          if (this.decay > 0){
+            this.decay -= .005;
+          } else {
+            this.decay = 0;
+          }
         }
       }
-      
     }
     // - - - - - - - - - - - - - - - - INIT PARTICLES - - - - - - - - - - - - - - - - -
 
-    // Create sheets of paper
     particles = new Array();
     sheets = new Array();
 
     for (var k=0; k<PARTICLES_COUNT; k++){
 
       var p = particles [k] = new Particle();
+
       //START POSITION
-      p.position = new THREE.Vector3(-40,-25-k*.5 + PARTICLES_COUNT*.5,0);
+      p.position =  new THREE.Vector3(-40,-20- 4/PARTICLES_COUNT*k, 0); //new THREE.Vector3(); //
       p.ID = k;
       p.rotation.x = 90;
-     //p.rotation.z = 45;
+     
 
-      p.setAvoidWalls( true );
-
+      //p.setAvoidWalls( true );
       p.setWorldSize( 500, 500, 200 );
    
       // generate geometry
-      var sheet = sheets[k] = new THREE.Mesh(new Hokusai( SHEET_WIDTH),new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: false } ) );
+      var sheet = sheets[k] = new THREE.Mesh(new Hokusai( SHEET_WIDTH, SHEET_SUBDIVISIONS),new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: false } ) );
 
       sheet.doubleSided = true;
       scene.add( sheet);
@@ -299,36 +320,54 @@ $(function() {
     document.addEventListener( 'touchmove', onDocumentTouchMove, false );
   }
 
+  // - - - - - - - - - - - - - - - - UPDATE PARTICLES - - - - - - - - - - - - - - - - -
+
   function updateParticles (){
     
     for (var k in particles){
-      
+      // blow 'em away
       if (counter==40) {
         particles[k].setblowAway(true);
-        //particles[k]._goal = new THREE.Vector3 (Math.random()*100, Math.random()*100, Math.random()*10)
-        //console.log("reposition")
       }
-      var nextGush = 70+(Math.floor (particles[k].seed*40) );
 
+      /*
+      var nextGush = 70+(Math.floor (particles[k].seed*40) );
       if (counter%nextGush < 2){
         //console.log("spiral")
         particles[k].turbulence();
 
       }
+      */
       
       
       sheets[k].position = particles[k].position;
       sheets[k].rotation = particles[k].rotation;
 
+      if (this.displace){
+        //update geo
+        var l = sheets[k].geometry.vertices.length;
+        var i=0;
+        while (i < l){
+            var s = sheets[k].geometry.vertices[i];
+            var p = particles[k];
+            var displaceDepth = .6;
+            //offset  wave displace
+            if (i%2==0){
+              s.z = Math.sin( counter* p.decay/10 + Math.PI*2*i/l* p.seed) * displaceDepth ;
+            } else {
+              s.z = Math.cos( counter* p.decay/10 + Math.PI*2*i/l* p.seed) * displaceDepth ;
+            }
+
+
+            i++;
+        }
+     }
+
       particles[k].run();
     }
      counter++;
-     //window.DRAG += window.DRAG-.01;
   }
 
-  this.setAvoidWalls = function ( value ) {
-     _avoidWalls = value;
-   }
 
   function animate() {
 
@@ -349,19 +388,11 @@ $(function() {
 
     renderer.render( scene, camera );
   }
+
   function onDocumentMouseMove(event) {
 
     mouse2D.x = ( event.clientX / window.innerWidth ) * 2 - 1;
     mouse2D.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-
-    var vector = new THREE.Vector3( event.clientX - SCREEN_WIDTH_HALF, - event.clientY + SCREEN_HEIGHT_HALF, 0 );
-
-     for (var k in particles){
-      //vector.z = particles[k].position.z;
-      //particles[k].repulse( vector);
-
-    }
 
   }
 
