2f5fd6b935afe60a06fe611ec4c39969c841a787
Hakan Ensari <hakan.ensari@papercavalier.com>
2012-09-19 22:52:13 +0100

    Minor edit to style

--- js/app.js
+++ js/app.js
@@ -1,19 +1,39 @@
 $(function() {
-  var $window = $(window);
-  var $logo = $('header h1');
-  var stretch = function() {
-    var height = $(window).height();
+  var $window = $(window),
+      $logo   = $('header h1'),
+      calculateDeathOfPrint,
+      setup;
+
+  (setup = function() {
+    var height = $window.height();
+    $logo.css('top', height / 2);
     $('header').css('min-height', height);
-    $('header h1').css('margin-top', height * 0.25);
-  }
-  stretch();
+  })();
   $window.resize(function() {
-    stretch();
+    setup();
   });
 
-  var calculateDeath = function() {
-    $('#death').text('about ' + (Math.random() * 7 + 6).toFixed(1));
-  }
-  calculateDeath();
-  setInterval(calculateDeath, 30000);
+  $('header a').click(function(event) {
+    $('html, body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 400);
+    event.preventDefault();
+  });
+
+  (calculateDeathOfPrint = function() {
+    var years = (Math.random() * 7 + 3).toFixed(1);
+    $('#death').text('about ' + years);
+  })();
+  setInterval(calculateDeathOfPrint, 30000);
+
+  $window.scroll(function() {
+    if($window.scrollTop() > $window.height() + 90) {
+      $('#eric').css({
+        'transform':         'rotate(360deg)',
+        '-moz-transform':    'rotate(360deg)',
+        '-webkit-transform': 'rotate(360deg)'
+      });
+      $window.unbind('scroll');
+    }
+  });
 });
