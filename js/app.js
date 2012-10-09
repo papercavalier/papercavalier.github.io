2095cb3cf54c3de0a992205c96f195b401fff967
Hakan Ensari <hakan.ensari@papercavalier.com>
2012-10-09 20:04:45 +0100

    Add Walter to team

--- js/app.js
+++ js/app.js
@@ -1,34 +1,3 @@
 $(function() {
-  var $window = $(window),
-      $logo   = $('header h1'),
-      setup;
-
-  (setup = function() {
-    var height = $window.height();
-    $logo.css('top', height / 2);
-    $('header').css('min-height', height);
-  })();
-  $window.resize(function() {
-    setup();
-  });
-
-  $('header a').click(function(event) {
-    $('html, body').stop().animate({
-        scrollTop: $($(this).attr('href')).offset().top
-    }, 400);
-    event.preventDefault();
-  });
-
-  $window.scroll(function() {
-    if($window.scrollTop() > $window.height() + 90) {
-      $('#eric').css({
-        'transform':         'rotate(360deg)',
-        '-moz-transform':    'rotate(360deg)',
-        '-webkit-transform': 'rotate(360deg)'
-      });
-      $window.unbind('scroll');
-    }
-  });
-
   $('a.thumbnail').tooltip();
 });
